4d11f9d54aadc5170124b97d6b719ebe95a72783
Manuel Paniagua <manuel.paniagua84@gmail.com>
2017-06-14 13:02:03 -0700

    First commit

---
+++ js/index.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+  var quote;
+  var author;
+  
+  function getQuote() {
+    $.ajax({
+      url: 'https://api.forismatic.com/api/1.0/',
+      jsonp: 'jsonp',
+      dataType: 'jsonp',
+      data: {
+        method: 'getQuote',
+        lang: 'en',
+        format: 'jsonp'
+      },
+      success: function(response) {
+        //console.log(response.quoteText);
+        quote = response.quoteText;
+        author = response.quoteAuthor;
+        
+        $('#quote').text(quote);
+        
+        if(author) {
+          $('#author').text('--' + author);
+        }
+        // if author empty
+        else {
+          $('#author').text('--Unknown ');
+        }
+      }
+    });
+     
+  }
+  // get quote on page load
+  getQuote();
+  // get quote on button click
+  $("#getQuote").on('click', function(event){
+        event.preventDefault();
+        getQuote();
+    });
+  // share quote on twitter
+  $("#share").on('click', function(event){
+      event.preventDefault();
+      window.open('https://twitter.com/intent/tweet?text=' + encodeURIComponent(quote + ' --' + author));
+  });
+});
